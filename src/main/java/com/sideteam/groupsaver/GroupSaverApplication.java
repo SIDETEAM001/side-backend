0095c3eab5cae47273588d96636ae27bfff081a9
220SRS <107467750+220SRS@users.noreply.github.com>
2023-10-08 15:28:07 +0900

    feat: project init

---
+++ src/main/java/com/sideteam/groupsaver/GroupSaverApplication.java
@@ -0,0 +1,13 @@
+package com.sideteam.groupsaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupSaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sideteam/groupsaver/GroupSaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideteam.groupsaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupSaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
