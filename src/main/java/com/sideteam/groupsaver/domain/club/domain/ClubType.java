739b81ba4b315be48c0f04707c84ca20d288462f
최재영 <107467750+220SRS@users.noreply.github.com>
2024-02-12 18:33:04 +0900

    Refactor[#63] : 클럽 생성 요청시 한글을 Enum으로 변환 (#68)
    Co-authored-by: Menhez Team <menhezteam@Menhezui-MacBookPro.local>

--- src/main/java/com/sideteam/groupsaver/domain/club/domain/ClubType.java
+++ src/main/java/com/sideteam/groupsaver/domain/club/domain/ClubType.java
@@ -1,6 +1,11 @@
 package com.sideteam.groupsaver.domain.club.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.sideteam.groupsaver.global.exception.BusinessException;
 import lombok.Getter;
+
+import static com.sideteam.groupsaver.global.exception.category.CategoryErrorCode.CATEGORY_SUB_NOT_FOUND;
+import static com.sideteam.groupsaver.global.exception.category.CategoryErrorCode.CLUB_TYPE_NOT_FOUND;
 
 @Getter
 public enum ClubType {
@@ -9,9 +14,19 @@ public enum ClubType {
     ONE("원데이")
     ;
 
-    private final String clubType;
+    private final String type;
 
-    ClubType(String clubType) {
-        this.clubType = clubType;
+    ClubType(String type) {
+        this.type = type;
+    }
+
+    @JsonCreator
+    public static ClubType getClubType(String type) {
+        for (ClubType clubType : ClubType.values()) {
+            if (clubType.getType().equals(type)) {
+                return clubType;
+            }
+        }
+        throw new BusinessException(CLUB_TYPE_NOT_FOUND, CLUB_TYPE_NOT_FOUND.getDetail());
     }
 }
