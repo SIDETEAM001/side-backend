7d366685efc0fc62d00565cdeb90a53698a091c0
Jaewon-pro <jaewonlee.pro@gmail.com>
2024-02-16 19:25:17 +0900

    Fix[#72]: 프로필 테스트 코드 수정 & 날짜 형식 변환 추가

--- src/main/java/com/sideteam/groupsaver/global/config/ObjectMapperConfig.java
+++ src/main/java/com/sideteam/groupsaver/global/config/ObjectMapperConfig.java
@@ -1,9 +1,17 @@
 package com.sideteam.groupsaver.global.config;
 
+import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static java.time.format.DateTimeFormatter.ofPattern;
 
 @Configuration
 public class ObjectMapperConfig {
@@ -11,8 +19,18 @@ public class ObjectMapperConfig {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.registerModule(javaDateTimeModule());
         return objectMapper;
+    }
+
+
+    private Module javaDateTimeModule() {
+        SimpleModule dateTimeModule = new SimpleModule();
+
+        dateTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(ofPattern("yyyy-MM-dd")));
+        dateTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(ofPattern("yyyy-MM-dd HH:mm:ss")));
+
+        return dateTimeModule;
     }
 
 }
